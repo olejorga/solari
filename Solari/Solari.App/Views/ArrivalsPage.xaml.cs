3650296e59fe2d8774a84169c120f1bcf02a9c39
olejorga <olejorga@hiof.no>
2022-05-22 17:21:51 +0200

    FEAT: Table transition logic works!

--- Solari/Solari.App/Views/ArrivalsPage.xaml.cs
+++ Solari/Solari.App/Views/ArrivalsPage.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using CommunityToolkit.Mvvm.DependencyInjection;
 
 using Microsoft.UI.Xaml.Controls;
-
+using Solari.App.Contracts.Services;
+using Solari.App.Helpers;
 using Solari.App.ViewModels;
+using Solari.Data.Access.Models;
+using Windows.Storage;
 
 namespace Solari.App.Views
 {
@@ -12,10 +15,23 @@ namespace Solari.App.Views
     {
         public ArrivalsViewModel ViewModel { get; }
 
+        private readonly INavigationService _navigationService;
+
         public ArrivalsPage()
         {
             ViewModel = Ioc.Default.GetService<ArrivalsViewModel>();
+            _navigationService = Ioc.Default.GetService<INavigationService>();
+
             InitializeComponent();
+        }
+
+        private async void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Flight flight = (Flight) DataGrid.SelectedItem;
+
+            await ApplicationData.Current.LocalSettings.SaveAsync("SelectedFlightNumber", flight.FlightNumber);
+
+            _navigationService.NavigateTo("Solari.App.ViewModels.FlightViewModel");
         }
     }
 }
--- Solari/Solari.App/Views/DeparturesPage.xaml.cs
+++ Solari/Solari.App/Views/DeparturesPage.xaml.cs
@@ -1,8 +1,13 @@
 ﻿using CommunityToolkit.Mvvm.DependencyInjection;
 
 using Microsoft.UI.Xaml.Controls;
-
+using Solari.App.Contracts.Services;
+using Solari.App.Helpers;
 using Solari.App.ViewModels;
+using Solari.Data.Access.Models;
+using System;
+using System.Diagnostics;
+using Windows.Storage;
 
 namespace Solari.App.Views
 {
@@ -12,10 +17,23 @@ namespace Solari.App.Views
     {
         public DeparturesViewModel ViewModel { get; }
 
+        private readonly INavigationService _navigationService;
+
         public DeparturesPage()
         {
             ViewModel = Ioc.Default.GetService<DeparturesViewModel>();
+            _navigationService = Ioc.Default.GetService<INavigationService>();
+
             InitializeComponent();
+        }
+
+        private async void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Flight flight = (Flight) DataGrid.SelectedItem;
+
+            await ApplicationData.Current.LocalSettings.SaveAsync("SelectedFlightNumber", flight.FlightNumber);
+
+            _navigationService.NavigateTo("Solari.App.ViewModels.FlightViewModel");
         }
     }
 }
